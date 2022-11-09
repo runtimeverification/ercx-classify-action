235580ed3d9b459ac670a559ea644d6c4df00477
Raoul Schaffranek <raoul.schaffranek@runtimeverification.com>
2022-11-09 10:18:57 +0100

    Initial classifier logic

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const exec = require('@actions/exec');
+const { promises: fs } = require('fs');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    core.info('Running classifier');
+    const infura_api_key = core.getInput('infura_api_key');
+    const testFile = 'test/ERC20PostDeploymentTest.sol';
+    const addressFile = 'lib/awesome-buggy-erc20-tokens/bad_tokens.top.json';
+    
+    // Read token list file
+    const addressFileContents = await fs.readFile(addressFile, 'utf8');
+    const addressJson = JSON.parse(addressFileContents);
+    const addresses = Object.keys(addressJson).slice(0, 3);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    // Call post deployment test for each address
 
-    core.setOutput('time', new Date().toTimeString());
+    for (let address of addresses) {
+
+      const options = {
+        ignoreReturnCode: true,
+        env : {
+          ERC20_ADDRESS : address
+        }
+      };
+
+      // Run forge test
+      const forgeTestOut = await exec.getExecOutput(
+        'forge',
+        [
+          'test',
+          '--silent',
+          '--match-path', testFile,
+          '--fork-url', `https://mainnet.infura.io/v3/${infura_api_key}`
+        ],
+        options
+      );
+      const testResult = forgeTestOut.stdout;
+
+    }
+    
+    if (!result) {
+      core.setFailed("One or more golden tests failed");
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
