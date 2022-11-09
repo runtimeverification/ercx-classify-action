4736fb035e93b49d6a65402d06d5802bde3c02cb
Raoul Schaffranek <raoul.schaffranek@runtimeverification.com>
2022-11-09 11:53:07 +0100

    Summrize results as bitstrings

--- index.js
+++ index.js
@@ -3,47 +3,76 @@ const exec = require('@actions/exec');
 const { promises: fs } = require('fs');
 
 
+const testFile = 'test/ERC20PostDeploymentTest.sol';
+const testContract = 'ERC20PostDeploymentTest';
+const testSuite = `${testFile}:${testContract}`;
+const addressFile = 'lib/awesome-buggy-erc20-tokens/bad_tokens.top.json';
+
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     core.info('Running classifier');
-    const infura_api_key = core.getInput('infura_api_key');
-    const testFile = 'test/ERC20PostDeploymentTest.sol';
-    const addressFile = 'lib/awesome-buggy-erc20-tokens/bad_tokens.top.json';
-    
+    const forgeList = await forgeTestList();
+    const testCases = forgeList[testFile][testContract];
     // Read token list file
-    const addressFileContents = await fs.readFile(addressFile, 'utf8');
-    const addressJson = JSON.parse(addressFileContents);
-    const addresses = Object.keys(addressJson).slice(0, 3);
+    const addresses = await readAddresses();
 
     // Call post deployment test for each address
-
+    let results = "";
     for (let address of addresses) {
-
-      const options = {
-        ignoreReturnCode: true,
-        env : {
-          ERC20_ADDRESS : address
-        }
-      };
-
-      // Run forge test
-      const forgeTestOut = await exec.getExecOutput(
-        'forge',
-        [
-          'test',
-          '--silent',
-          '--match-path', testFile,
-          '--fork-url', `https://mainnet.infura.io/v3/${infura_api_key}`
-        ],
-        options
-      );
-      const testResult = forgeTestOut.stdout;
-
+      const testResult = await forgeTest(address);
+      const results = testResult[testSuite].test_results;
+      let resultBitString = "";
+      for (let [testName, result] of Object.entries(results)) {
+        resultBitString += result.success ? "1" : "0";
+      }
+      results += `${address}:${resultBitString}\n`;
     }
+    core.info("Results");
+    core.info(testCases.join("\n"));
+    core.info(results);
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function forgeTest(address) {
+  const infura_api_key = core.getInput('infura_api_key');
+  const options = {
+    ignoreReturnCode: true,
+    env : {
+      ERC20_ADDRESS : address
+    }
+  };
+  const forgeTestOut = await exec.getExecOutput(
+    'forge',
+    [
+      'test',
+      '--silent',
+      '--json',
+      '--match-path', testFile,
+      '--fork-url', `https://mainnet.infura.io/v3/${infura_api_key}`
+    ],
+    options
+  );
+  const forgeTestJson = JSON.parse(forgeTestOut.stdout);
+  return forgeTestJson;
+}
+
+async function forgeTestList() {
+  const forgeListOut = await exec.getExecOutput(
+    'forge',
+    ['test', '--list', '--json', '--silent']
+  );
+  const forgeListJson = JSON.parse(forgeListOut.stdout);
+  return forgeListJson;
+}
+
+async function readAddresses() {
+  const addressFileContents = await fs.readFile(addressFile, 'utf8');
+  const addressJson = JSON.parse(addressFileContents);
+  const addresses = Object.keys(addressJson).slice(0, 3);
+  return addresses;
 }
 
 run();
